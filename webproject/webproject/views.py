c44f12efe012214ba72c601b2274ca63b92910fd
gagan <131556549+gfoc@users.noreply.github.com>
2024-05-14 15:19:55 +0530

    Update views.py

--- webproject/webproject/views.py
+++ webproject/webproject/views.py
@@ -1,2 +1,15 @@
+from django.http import HttpResponse
+
 def aboutus(request):
-  return httpResponse("Welcome to mysite")
+    return HttpResponse("<b>Welcome to webproject<b/>")
+
+def homepage(request):
+    return HttpResponse("Welcome to the Homepage")
+
+
+def Course(request):
+    return HttpResponse("welcometo the course section")
+
+
+def coursedetails(request,courseid):
+    return HttpResponse(courseid)
