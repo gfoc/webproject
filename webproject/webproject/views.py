f9024689d0f093c97cc5a5143a34ddb5c71e99aa
gagan <131556549+gfoc@users.noreply.github.com>
2024-05-15 23:32:34 +0530

    Update views.py

--- webproject/webproject/views.py
+++ webproject/webproject/views.py
@@ -4,6 +4,14 @@ def aboutus(request):
     return HttpResponse("<b>Welcome to webproject<b/>")
 
 def homepage(request):
+    data ={
+        'title':'Home Page",
+        'about':'This is professional website made for doing specifications and excellaration in different fields',
+        'clist':['PHP','JAVA','PYTHON'],
+    }
+
+    return render(request,"index.html",data)
+    
     return HttpResponse("Welcome to the Homepage")
 
 
