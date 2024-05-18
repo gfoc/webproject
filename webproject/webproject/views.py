da431e17cdfc91c271779364e320d7061635cd9d
gagan <131556549+gfoc@users.noreply.github.com>
2024-05-18 16:19:22 +0530

    commit

--- webproject/webproject/views.py
+++ webproject/webproject/views.py
@@ -12,6 +12,7 @@ def homepage(request):
         'title':'Home..Page',
         'about':'Hello this is a professional website',
         'courselist':['PHP','JAVA','PYTHON'],
+        'numbers':[10,20,30,40,50],
         'students_details':[
             {'name':'pradeep','phone':9374838282},
             {'name':'testing','phone':4783384848}
